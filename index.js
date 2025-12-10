dfb51b7e30bb0db4abf7843e44ebee2c6306b1b5
harold12 <dpaboncr7@gmail.com>
2025-12-09 20:30:44 -0500

    hola mundo render

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo desde Render!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
